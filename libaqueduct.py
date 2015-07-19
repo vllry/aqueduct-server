b90b58cf4ac0afa9a7bc9aa7e2e2cd0a5d367d07
Vallery Lancey <vallery@messages.xyz>
2015-07-18 20:45:16 -0700

    Switched from INI-style config files to JSON

---
+++ libaqueduct.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+from os import listdir, path
+import tarfile
+import json
+
+
+
+class config:
+	"""Load and process the config files"""
+
+
+
+	def __init__(self, config_file_path = "/home/vallery/Development/Aqueduct/aqueduct-server/etc/aqueduct-server/aqueduct-server.conf"):
+		attributes = [
+						'arch',
+						'components',
+						'distributions'
+					]
+		self.repos = {}
+
+		conf = json.load(open(config_file_path, 'r'))
+		repo_conf = json.load(open(conf['repositories'], 'r'))
+
+		for repo in repo_conf:
+			self.repos[repo] = {}
+			for release in repo_conf[repo]['releases']:
+				self.repos[repo][release] = repo_conf[repo]['releases'][release]
+				for attribute in attributes:
+					if attribute not in self.repos[repo][release] and attribute in repo_conf[repo]['defaults']:
+						self.repos[repo][release][attribute] = repo_conf[repo]['defaults'][attribute]
+
+
+
+def var_replace(s, values_original):
+	"""Replaces all instances in s (regardless of case) of {{KEY}} with values_original[KEY]"""
+
+	values = {}
+	for key in values_original.keys(): #Add brackets around key values
+		values['{{'+key+'}}'] = values_original[key]
+
+	s_lower = s.lower()
+	for key in values.keys():
+		pos = s_lower.find(key)
+		while pos != -1:
+			old = s[pos:pos+len(key)]
+			s = s.replace(old, values[key])
+			s_lower = s.lower()
+			pos = s_lower.find(key)
+	return s
--- old-libaqueduct.py
+++ old-libaqueduct.py
@@ -2,45 +2,6 @@ from os import popen
 
 
 
-class config:
-	"""Load and process the config files"""
-
-
-	def __init__(self, config_file_path = "/home/vallery/Desktop/conduit/aqueduct/etc/aqueduct/repositories.conf"):
-		from os import listdir, path
-		from configparser import ConfigParser
-
-		attributes = [	'arch',
-						'components',
-						'distributions']
-		self.repos = {}
-		self.releases = {}
-
-		rootconfig = ConfigParser()
-		rootconfig.read(config_file_path)
-		for os in rootconfig.sections():
-			self.repos[os] = {
-							'dev' : rootconfig[os]['dev'].replace(' ', '').split(','),
-							'supported' : rootconfig[os]['supported'].replace(' ', '').split(','),
-							'all' : rootconfig[os]['all'].replace(' ', '').split(','),
-							}
-			self.releases[os] = {}
-
-			repoconfig = ConfigParser()
-			repoconfig.read('/home/vallery/Desktop/conduit/aqueduct/etc/aqueduct/releases/'+os+'.conf')
-			for release in repoconfig.sections():
-				self.releases[os][release] = {}
-
-				for attr in attributes:
-					if attr in repoconfig[release]:
-						self.releases[os][release][attr] = repoconfig[release][attr].replace(' ', '').replace('[RELEASE]', release).split(',')
-					elif attr in rootconfig[os]:
-						self.releases[os][release][attr] = rootconfig[os][attr].replace(' ', '').replace('[RELEASE]', release).split(',')
-					else:
-						print("uh oh, couldn't find " + attr)
-
-
-
 class repo_deb:
 	"""Manage the deb repositories"""
 
@@ -60,22 +21,3 @@ class repo_deb:
 
 	def add(self, repo, file):
 		popen('aptly repo add ' + repo + ' ' + file)
-
-
-
-def str_replace(s, values_original):
-	"""Replaces all instances in s (regardless of case) of KEY with values[KEY]"""
-
-	values = {}
-	for key in values_original.keys(): #Add brackets around key values
-		values['{'+key+'}'] = values_original[key]
-
-	s_lower = s.lower()
-	for key in values.keys():
-		pos = s_lower.find(key)
-		while pos != -1:
-			old = s[pos:pos+len(key)]
-			s = s.replace(old, values[key])
-			s_lower = s.lower()
-			pos = s_lower.find(key)
-	return s
